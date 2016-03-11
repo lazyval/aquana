c069a31db27c24ee50545800cb53bd9ed025a6dd
WonderBeat <dgolovachev@griddynamics.net>
2016-03-11 13:49:21 +0300

    prettify stacktrace

--- src/main/kotlin/org/wonderbeat/RetryListeners.kt
+++ src/main/kotlin/org/wonderbeat/RetryListeners.kt
@@ -3,12 +3,15 @@ package org.wonderbeat
 import com.github.rholder.retry.Attempt
 import com.github.rholder.retry.RetryListener
 import org.slf4j.LoggerFactory
+import java.io.PrintWriter
+import java.io.StringWriter
 
 val logAttemptFailure = object: RetryListener {
     private val logger = LoggerFactory.getLogger("org.wonderbeat.retry")
     override fun <V : Any?> onRetry(attempt: Attempt<V>?) {
         if(attempt?.hasException() == true) {
-            logger.warn("Attempt ${attempt?.attemptNumber} failed ${attempt?.exceptionCause} with stacktrace: ${attempt?.exceptionCause?.stackTrace}")
+            val stacktrace = attempt?.exceptionCause?.printStackTrace(PrintWriter(StringWriter()));
+            logger.warn("Attempt ${attempt?.attemptNumber} failed ${attempt?.exceptionCause} with stacktrace $stacktrace")
         }
     }
 }
