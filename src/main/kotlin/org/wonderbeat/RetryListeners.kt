f1f4c9ee800789614f8ef86fafaee41472059782
WonderBeat <dgolovachev@griddynamics.net>
2016-03-11 13:53:39 +0300

    typo in stackwriter

--- src/main/kotlin/org/wonderbeat/RetryListeners.kt
+++ src/main/kotlin/org/wonderbeat/RetryListeners.kt
@@ -10,8 +10,9 @@ val logAttemptFailure = object: RetryListener {
     private val logger = LoggerFactory.getLogger("org.wonderbeat.retry")
     override fun <V : Any?> onRetry(attempt: Attempt<V>?) {
         if(attempt?.hasException() == true) {
-            val stacktrace = attempt?.exceptionCause?.printStackTrace(PrintWriter(StringWriter()));
-            logger.warn("Attempt ${attempt?.attemptNumber} failed ${attempt?.exceptionCause} with stacktrace $stacktrace")
+            val stringWriter = StringWriter()
+            attempt?.exceptionCause?.printStackTrace(PrintWriter(stringWriter));
+            logger.warn("Attempt ${attempt?.attemptNumber} failed ${attempt?.exceptionCause} with stacktrace ${stringWriter.toString()}")
         }
     }
 }
