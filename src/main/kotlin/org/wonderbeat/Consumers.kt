c5d110b44680deae6592edab0c0d25c0cf08913c
WonderBeat <dgolovachev@griddynamics.net>
2016-05-23 12:34:21 +0300

    exponential retryer backoff

--- src/main/kotlin/org/wonderbeat/Consumers.kt
+++ src/main/kotlin/org/wonderbeat/Consumers.kt
@@ -3,11 +3,13 @@ package org.wonderbeat
 import com.github.rholder.retry.Retryer
 import com.github.rholder.retry.RetryerBuilder
 import com.github.rholder.retry.StopStrategies
+import com.github.rholder.retry.WaitStrategies
 import kafka.api.FetchRequestBuilder
 import kafka.common.ErrorMapping
 import kafka.consumer.SimpleConsumer
 import kafka.message.ByteBufferMessageSet
 import org.slf4j.LoggerFactory
+import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicLong
 
 private val logger = LoggerFactory.getLogger("org.wonderbeat.consumers")
@@ -16,6 +18,7 @@ class RetryingConsumer(private val delegate: MonotonicConsumer,
                        val retryer: Retryer<ByteBufferMessageSet> = RetryerBuilder.newBuilder<ByteBufferMessageSet>()
                                             .retryIfException()
                                             .withRetryListener(logAttemptFailure)
+                                            .withWaitStrategy(WaitStrategies.exponentialWait(2, 10, TimeUnit.SECONDS))
                                             .withStopStrategy(StopStrategies.stopAfterAttempt(5))
                                             .build()): MonotonicConsumer by delegate {
     override fun fetch(): ByteBufferMessageSet = retryer.call {  delegate.fetch() }
